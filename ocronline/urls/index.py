db01408b37a65cb0a1174f8043a33c6ae08d2031
ylz <1914776325@qq.com>
2022-07-18 00:28:18 +0800

    Add comments about the File's structure

--- ocronline/urls/index.py
+++ ocronline/urls/index.py
@@ -1,11 +1,15 @@
 # 将该路径下所有文件夹里的python文件include进来
 
+# 本文件由总的praapp里的urls文件路由过来
+# 接着再去路由到别的网址
+
+
 from django.urls import path, include
 from ocronline.views.index import index
 
 
 urlpatterns = [
-    path("", index, name="index"), # 总函数不需要加路径 直接可以访问
+    path("", index, name="index"), # 总函数不需要加路径 直接可以访问views文件夹里的index.py文件的index函数
     path("menu/", include("ocronline.urls.menu.index")),
     path("playground/", include("ocronline.urls.playground.index")),
     path("settings", include("ocronline.urls.settings.index")),
--- ocronline/views/index.py
+++ ocronline/views/index.py
@@ -1,7 +1,9 @@
 # 在web端被调用 返回web.html
+# 被urls文件里的index.py调用
 
 from django.shortcuts import render  # 服务器端渲染html文件
 
-def index(request):
-    retunr render(request, "multiends/web.html")
 
+# render函数返回一个字符串到用户前端 也即html页面
+def index(request):
+    return render(request, "multiends/web.html")
